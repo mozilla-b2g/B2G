22fd84c3fdfffd1c2b5894d344747db64baa9848
Michael Wu <mwu@mozilla.com>
2012-05-08 22:11:36 -0700

    Add support for x86 emulator in run-emulator.sh

--- run-emulator.sh
+++ run-emulator.sh
@@ -1,25 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 B2G_HOME=$PWD
 
-DBG_CMD=""
-if [ x"$DBG" != x"" ]; then
-   DBG_CMD="gdb -args"
-fi
-TAIL_ARGS=""
-if [ x"$GDBSERVER" != x"" ]; then
-   TAIL_ARGS="$TAIL_ARGS -s -S"
-fi
+. .config
+
+DEVICE=${DEVICE:-generic}
 
 TOOLS_PATH=$B2G_HOME/out/host/`uname -s | tr "[[:upper:]]" "[[:lower:]]"`-x86/bin
 
+DBG_CMD=""
+if [ x"$DBG" != x"" ]; then
+    DBG_CMD="gdb -args"
+fi
+TAIL_ARGS=""
+if [ x"$GDBSERVER" != x"" ]; then
+    TAIL_ARGS="$TAIL_ARGS -s -S"
+fi
+
+if [ "$DEVICE" = "generic_x86" ]; then
+    EMULATOR=$TOOLS_PATH/emulator-x86
+    KERNEL=$B2G_HOME/prebuilts/qemu-kernel/x86/kernel-qemu
+else
+    EMULATOR=$TOOLS_PATH/emulator
+    KERNEL=$B2G_HOME/prebuilts/qemu-kernel/arm/kernel-qemu-armv7
+    TAIL_ARGS="$TAIL_ARGS -cpu 'cortex-a8'"
+fi
+
 export PATH=$PATH:$TOOLS_PATH
-${DBG_CMD} $TOOLS_PATH/emulator \
-   -kernel $B2G_HOME/prebuilts/qemu-kernel/arm/kernel-qemu-armv7 \
-   -sysdir $B2G_HOME/out/target/product/generic/ \
-   -data $B2G_HOME/out/target/product/generic/userdata.img \
+${DBG_CMD} $EMULATOR \
+   -kernel $KERNEL \
+   -sysdir $B2G_HOME/out/target/product/$DEVICE/ \
+   -data $B2G_HOME/out/target/product/$DEVICE/userdata.img \
    -memory 512 \
    -partition-size 512 \
    -skindir $B2G_HOME/development/tools/emulator/skins \
    -skin WVGA854 \
    -verbose \
-   -qemu -cpu 'cortex-a8' $TAIL_ARGS
+   -qemu $TAIL_ARGS
