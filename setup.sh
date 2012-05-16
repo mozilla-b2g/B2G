e202251763d351f0ae0be1e5fee9fcdc22702ff9
vagrant <vagrant@ubuntu-oneiric.(none)>
2012-05-16 06:09:43 +0100

    Export gaia domain/debug options

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ LUNCH=${LUNCH:-full_${DEVICE}-eng}
 export USE_CCACHE=yes &&
 export GECKO_PATH &&
 export GAIA_PATH &&
+export GAIA_DOMAIN &&
+export GAIA_PORT &&
+export GAIA_DEBUG &&
 export GECKO_OBJDIR &&
 . build/envsetup.sh &&
 lunch $LUNCH
