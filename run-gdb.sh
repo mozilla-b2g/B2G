bf649a86f5f94a19f00a1499929598f3b67add5e
Dave Hylands <dhylands@gmail.com>
2012-09-11 19:58:45 -0700

    Remove all usage of awk from the run-gdb.sh script
    Did some additional cleanup as well.
    Use the namefilter capability of ps rather than invoking grep
    Replace backticks with $()
    Use read to parse the pid rather than using awk

--- run-gdb.sh
+++ run-gdb.sh
@@ -4,15 +4,14 @@ SCRIPT_NAME=$(basename $0)
 . load-config.sh
 
 ADB=adb
-GDB=${GDB:-prebuilt/`uname -s | tr "[[:upper:]]" "[[:lower:]]"`-x86/toolchain/arm-linux-androideabi-4.4.x/bin/arm-linux-androideabi-gdb}
+GDB=${GDB:-prebuilt/$(uname -s | tr "[[:upper:]]" "[[:lower:]]")-x86/toolchain/arm-linux-androideabi-4.4.x/bin/arm-linux-androideabi-gdb}
 B2G_BIN=/system/b2g/b2g
-GDBINIT=/tmp/b2g.gdbinit.`whoami`
+GDBINIT=/tmp/b2g.gdbinit.$(whoami)
 
 GONK_OBJDIR=out/target/product/$DEVICE
 SYMDIR=$GONK_OBJDIR/symbols
 
-GDBSERVER_PID=`$ADB shell toolbox ps |
-               grep "gdbserver" | awk '{ print \$2; }'`
+GDBSERVER_PID=$($ADB shell 'toolbox ps gdbserver | (read header; read user pid rest; echo $pid)')
 
 GDB_PORT=$((10000 + $(id -u) % 50000))
 if [ "$1" = "attach"  -a  -n "$2" ] ; then
@@ -25,7 +24,7 @@ if [ "$1" = "attach"  -a  -n "$2" ] ; then
    # cmdline is null separated
    B2G_BIN=$($ADB shell cat /proc/$B2G_PID/cmdline | tr '\0' '\n' | head -1)
 else
-   B2G_PID=`$ADB shell toolbox ps | grep -v "plugin-container" | grep "b2g" | awk '{ print \$2; }'`
+   B2G_PID=$($ADB shell 'toolbox ps b2g | (read header; read user pid rest; echo $pid)')
 fi
 
 for p in $GDBSERVER_PID ; do
