0b40829feed622ca387f8b0917aaa695b61d4794
Justin Lebar <justin.lebar@gmail.com>
2012-12-10 14:27:39 -0500

    During flash.sh, remove any regular files on the device which aren't in dist/b2g on the host.

--- flash.sh
+++ flash.sh
@@ -147,6 +147,32 @@ flash_heimdall()
 	echo Run \|./flash.sh gaia\| if you wish to install or update gaia.
 }
 
+# Delete files in the device's /system/b2g that aren't in
+# $GECKO_OBJDIR/dist/b2g.
+#
+# Note that we do not delete *folders* in /system/b2g.  This is intentional,
+# because some user data is stored under /system/b2g (e.g. prefs), but it seems
+# to be stored only inside directories.
+delete_extra_gecko_files_on_device()
+{
+	files_to_remove="$(cat <(ls "$GECKO_OBJDIR/dist/b2g") <(run_adb shell "ls /system/b2g" | tr -d '\r') | sort | uniq -u)"
+	if [[ "$files_to_remove" != "" ]]; then
+		# We expect errors from the call to rm below under two circumstances:
+		#
+		#  - We ask rm to remove a directory (per above, we don't
+		#    actually want to remove directories, so rm is doing the
+		#    right thing by not removing dirs)
+		#
+		#  - We ask rm to remove a file which isn't on the device (if
+		#    you squint at files_to_remove, you'll see that it will
+		#    contain files which are on the host but not on the device;
+		#    obviously we can't remove those files from the device).
+
+		run_adb shell "cd /system/b2g && rm $files_to_remove" > /dev/null
+	fi
+	return 0
+}
+
 while [ $# -gt 0 ]; do
 	case "$1" in
 	"-s")
@@ -164,6 +190,7 @@ done
 case "$PROJECT" in
 "gecko")
 	run_adb remount &&
+	delete_extra_gecko_files_on_device &&
 	run_adb push $GECKO_OBJDIR/dist/b2g /system/b2g &&
 	echo Restarting B2G &&
 	run_adb shell stop b2g &&
