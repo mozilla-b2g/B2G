2f5e1493dfa846c68877060c705f3c0b3c22f3dd
John Ford <john@johnford.info>
2013-01-09 13:53:11 +0100

    remove vendorsetup and treeid to move to gonk-patches

--- build.sh
+++ build.sh
@@ -53,7 +53,7 @@ function configure_device() {
 }
 
 export REPO=$PWD/repo
-export B2G_TREEID_SH="$PWD/treeid.sh"
+export B2G_TREEID_SH="$PWD/patches/treeid.sh"
 export B2G_HASHED_FILES="vendorsetup.sh ${B2G_TREEID_SH}"
 export B2G_PATCH_DIRS_OVERRIDE=patches
 
