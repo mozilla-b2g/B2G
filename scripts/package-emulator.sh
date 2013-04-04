fc4a2b87306d22d094f3d8114b5b8e903059ffc1
Jonathan Griffin <jgriffin@mozilla.com>
2013-04-04 13:38:06 -0700

    bug 855003 - script to package emulator r=jhford

---
+++ scripts/package-emulator.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -
+cd ..
+. setup.sh
+PRODUCT_OUT=$(get_build_var PRODUCT_OUT)
+HOST_OUT=$(get_build_var HOST_OUT)
+TOP=$(pwd)
+
+EMULATOR_FILES=(\
+	.config \
+	load-config.sh \
+	run-emulator.sh \
+	${HOST_OUT}/bin/adb \
+	${HOST_OUT}/bin/emulator \
+	${HOST_OUT}/bin/emulator-arm \
+	${HOST_OUT}/bin/mksdcard \
+	${HOST_OUT}/bin/qemu-android-x86 \
+	${HOST_OUT}/lib \
+	${HOST_OUT}/usr \
+	development/tools/emulator/skins \
+	prebuilts/qemu-kernel/arm/kernel-qemu-armv7 \
+	${PRODUCT_OUT}/system/build.prop \
+	${PRODUCT_OUT}/system.img \
+	${PRODUCT_OUT}/userdata.img \
+	${PRODUCT_OUT}/ramdisk.img \
+	${PRODUCT_OUT}/hardware-qemu.ini)
+
+EMULATOR_ARCHIVE="${TOP}/out/emulator.tar.gz"
+
+echo "Creating emulator archive at $EMULATOR_ARCHIVE"
+
+rm -rf $EMULATOR_ARCHIVE
+tar -cvzf $EMULATOR_ARCHIVE --transform 's,^,b2g-distro/,S' --show-transformed-names ${EMULATOR_FILES[@]}
+
