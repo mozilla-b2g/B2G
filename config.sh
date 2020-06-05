6b382a10757ceebf20864cc4ef51b6683e42cba7
Alexandre <alexandre@serveur>
2020-06-05 21:56:48 +0200

    Use device-specific objdir

--- config.sh
+++ config.sh
@@ -71,7 +71,7 @@ if [ -n "$2" ]; then
 fi
 
 echo MAKE_FLAGS=-j$((CORE_COUNT + 2)) > .tmp-config
-echo GECKO_OBJDIR=$PWD/objdir-gecko >> .tmp-config
+echo GECKO_OBJDIR=$PWD/objdir-gecko-\$PRODUCT_NAME >> .tmp-config
 echo DEVICE_NAME=$1 >> .tmp-config
 
 case "$1" in
